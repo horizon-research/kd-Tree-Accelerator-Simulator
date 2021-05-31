0fd438bbe9d1a149477db9e463dc384b85f7ae99
gunnhamm <ghammon5@u.rochester.edu>
2021-05-31 13:07:05 -0400

    Fixed main driver

--- trace_generator/main.cpp
+++ trace_generator/main.cpp
@@ -7,67 +7,67 @@
 #include "KD_Tree.h"
 #include <fstream> 
 #include <vector>
+using namespace std;
 int main(int argc, char* argv[]) {
  
 
     if (argc != 4) {
-        std::cout << "Invalid number of arguments\n";
+        cout << "Invalid number of arguments\n";
         return 1;
     }
     KD_Tree tree(argv[1], argv[3]);
-    float range[3][2] = {{4, 5}, {0, 1}, {6, 7}};
-    std::vector<Point*> points = tree.range_search(range);
-    for (auto p : points) {
-        std::cout << *p << std::endl;
-    }
-    /*
-    std::ifstream fin("../Search_Inputs/" + std::string(argv[2]));
+    ifstream fin("../Search_Inputs/" + string(argv[2]));
 
     if (fin.is_open()) {
-        std::string line;
+        string line;
         float value;
         //kd-tree dimension found
         getline(fin, line);
         int num_dimensions = stoi(line);
         if (num_dimensions != tree.get_num_dimensions()) {
-            std::cout << "Tree and Search dimensions do not match" << std::endl;
+            cout << "Tree and Search dimensions do not match" << endl;
             exit(0);
         }
         //Each line of file is read
         while (getline(fin, line)) {
-            std::istringstream sin(line);
-            std::string query_type;
+            istringstream sin(line);
+            string query_type;
             sin >> query_type;
             float values[num_dimensions];
-            bool KNN = false;
-            if (query_type == "KNN") {
-                KNN = true;
-            }
-            //Point is created and added to tree
-            for (int i = 0; i < num_dimensions; i++) {
-                sin >> value;
-                values[i] = value;
-            }
-            Point query(num_dimensions, values);
-            if (KNN) {
-                int k;
-                sin >> k;
-                std::vector<Point*> points = tree.knn_search(query, k);
+            if (query_type == "RANGE") {
+                float range[3][2];
+                sin >> range[0][0] >> range[0][1] 
+                    >> range[1][0] >> range[1][1] 
+                    >> range[2][0] >> range[2][1];  
+                vector<Point*> points = tree.range_search(range);
                 for (auto p : points) {
-                    std::cout << *p << std::endl;
-                }
+                    cout << *p << endl;
+                }   
             }
             else {
-                Point* p = tree.nearest_neighbour(query);
-                std::cout << *p << std::endl;
-            }
-            
+                for (int i = 0; i < num_dimensions; i++) {
+                    sin >> value;
+                    values[i] = value;
+                }
+                Point query(num_dimensions, values);
+                if (query_type == "KNN") {
+                    int k;
+                    sin >> k;
+                    vector<Point*> points = tree.knn_search(query, k);
+                    for (auto p : points) {
+                        cout << *p << endl;
+                    }
+                }
+                else {
+                    Point* p = tree.nearest_neighbour(query);
+                    cout << *p << endl;
+                }
+            } 
         }
     }
     else {
-        std::cout << "Error opening file: " << argv[2] << std::endl;
+        cout << "Error opening file: " << argv[2] << endl;
         exit(0);
     }
-    */
     return 0;
 }
