116ac877363d59c51d5f91d7ee2c75cb6e7c29bc
gunnhamm <ghammon5@u.rochester.edu>
2021-07-19 13:44:28 -0400

    Added basics of bucket kd-tree

--- scratchpad_sim/new_kd_tree.py
+++ scratchpad_sim/new_kd_tree.py
@@ -3,6 +3,7 @@ from query import Query
 from point import Point
 import time
 from collections import deque
+from memory import Memory
 #Constants which make trace writes more readable
 data_sizes = [40, 40, 64]
 
@@ -395,44 +396,197 @@ class KD_Tree:
                     self.knn_ideal_rec(query, current_best, k, tree.left, level + 1)
         
 
-    class Bucket_KD_Tree:
-        def __init__(self, data_in):
-            file = open(data_in)
-            lines = file.readlines()
-            points = []
-            self.buckets = {}
-            for line in file:
-                tokens = line.split()
-                p = Point(tokens)
-                points.append(p)
-        def build_toptree(self, points, lo, hi, level, max):
-            #Median for sublist is found
-            median = int((lo + hi) / 2)
-            #Dimension to sort on
-            dim = level % 3
-            #Sublist is sorted based on current dimension
-            points[lo:hi + 1] = sorted(points[lo:hi + 1], key=lambda p: p.values[dim])
-            #Median point is inserted into tree
-            p = points[median]
-            n = Node(p)
-            if level < max:
-                #Function called recursivley to all points to the left and right of the median in the sublist, sorting dimension is incremented
-                n.left = self.build_tree(points, lo, median - 1, level + 1)
-                n.right = self.build_tree(points, median + 1, hi, level + 1)
-                return n
+class Bucket_KD_Tree:
+    def insert(self, tree, p, level):
+        if tree.right  ==  None and tree.left == None:
+            self.buckets[tree].append(p)
+            self.point_indices[p] = self.num_points
+            self.num_points += 1
+        else:
+            dimension = level % 3
+            if tree.p.dim_value(dimension) > p.dim_value(dimension):
+                self.insert(tree.left, p, level + 1)
             else:
-                self.buckets[n] = []
-                return n
-        def insert(self, tree, p, level):
-            if tree.right  ==  None and tree.left == None:
-                self.buckets[tree].append(p)
+                self.insert(tree.right, p, level + 1)
+    def __init__(self, data_in, levels):
+        file = open(data_in)
+        lines = file.readlines()
+        points = []
+        self.point_indices = {}
+        self.toptree_point_indices = {}
+        self.memory = None
+        self.num_points = 0
+        self.toptree_size = 0
+        self.levels = levels
+        self.buckets = {}
+        for line in lines:
+            tokens = line.split()
+            p = Point(tokens)
+            points.append(p)
+        self.root = self.build_toptree(points, 0, len(points) - 1, 0, levels)
+        for p in points:
+            self.insert(self.root, p, 0)
+        self.print_tree_rec(self.root, 0, False)
+    def build_toptree(self, points, lo, hi, level, max):
+        #Median for sublist is found
+        print(len(points))
+        median = int((lo + hi) / 2)
+        #Dimension to sort on
+        dim = level % 3
+        #Sublist is sorted based on current dimension
+        points[lo:hi + 1] = sorted(points[lo:hi + 1], key=lambda p: p.values[dim])
+        #Median point is inserted into tree
+        p = points[median]
+        n = Node(p)
+        self.toptree_point_indices[n] = self.toptree_size
+        self.toptree_size += 1
+        if level < max:
+            #Function called recursivley to all points to the left and right of the median in the sublist, sorting dimension is incremented
+            n.left = self.build_toptree(points, lo, median - 1, level + 1, max)
+            n.right = self.build_toptree(points, median + 1, hi, level + 1, max)
+            return n
+        else:
+            self.buckets[n] = []
+            return n
+    
+    def print_tree_rec(self, tree, level, sub):
+        time.sleep(0.01)
+        if tree.left == None and tree.right == None:
+            
+            for i in range(level):
+                print("  ", end='')
+            for p in self.buckets[tree]:
+                print(p, end=' ')
+            print()
+            
+        else:
+            for i in range(level):
+                print("  ", end='')
+            
+            print(tree.p)
+            self.print_tree_rec(tree.left, level + 1, sub)
+            self.print_tree_rec(tree.right, level + 1, sub)
+    def knn(self, query, k):
+        #Stack is initialized to track recursive calls
+        self.stack = []
+        self.stack.append(0)
+        current_best = []
+        #Recursive function modifies current best, when finished, it will contain the k nearest points in the tree, and their distances from the query point
+        self.knn_rec(query, current_best, k, self.root, 0)
+        return current_best
+    def knn_rec(self, query, current_best, k, tree, level):
+        
+        call = self.stack[-1]
+        #RS
+        self.access(READ, QUERY, call, 0) 
+        
+        self.computation(1)
+        if tree:
+            self.computation(2)
+            if tree in self.buckets:
+                self.exhaustive_search(query, current_best, self.buckets[tree], k)
+                return
+            #RP
+            self.access(READ, POINT, self.point_indices[tree.p], 0)
+
+            #CD
+            splitting_plane = level % self.num_dimensions
+
+            #Values found for current point and target point
+            query_val = query.dim_value(splitting_plane)
+            current_val = tree.p.dim_value(splitting_plane)
+
+            #Current point's distance to query found
+            
+            #Distance stored as negative to convert heap to max heap
+            self.computation(18)
+            distance = -query.distance(tree.p)
+            current = (distance, tree.p)
+
+            #If there are already k points in heap, add current point only if its distance is less than the farthest away point in heap
+            self.computation(4)
+            if len(current_best) == k:
+                if distance > current_best[0][0]:
+                    heapq.heapreplace(current_best, current)
+            #Otherwise, add
             else:
-                dimension = level % 3
-                if tree.p.dim_value(dimension) > p.dim_value(dimension):
-                    self.insert(tree.left, p, level + 1)
-                else:
-                    self.insert(tree.right, p, level + 1)
-                    
+                heapq.heappush(current_best, current)
+            self.computation(2)
+            if tree.right == None and tree.left == None:
+                self.backtrack()
+                self.access(READ, QUERY, self.stack.pop(), 32)
+                
+                return
+
+            #NT 
+            #If target value is less than current, take left subtree
+            self.computation(2)
+            if  query_val < current_val:
+                self.stack.append(call + 1)
+                self.access(WRITE, QUERY, call + 1, 0)
+                self.computation(1)
+                self.knn_rec(query, current_best, k, tree.left, level + 1)
+
+                #BT
+                #If the current best distance + the target value is greater than the current value,
+                #it is possible that the closest point could be contained in right subtree, so it is searched as well
+                if self.approximation != 0:
+                    self.computation(6)
+                    if (query_val + current_best[0][0] > current_val or k > len(current_best)):
+                        self.stack.append(call + 1)
+                        self.access(WRITE, QUERY, call + 1, 0)
+                        self.backtrack()
+                        self.knn_rec(query, current_best, k, tree.right, level + 1)
+                
+            #If target value is greater than current, take right subtree
+            else:
+                self.stack.append(call + 1)
+                self.access(WRITE, QUERY, call + 1, 0)
+                self.computation(1)
+                self.knn_rec(query, current_best, k, tree.right, level + 1)
+
+                #BT
+                #If the target value - the current best distance is less than than the current value,
+                #it is possible that the closest point could be contained in the left subtree, so it is searched as well
+                if self.approximation != 0:
+                    self.computation(6)
+                    if (query_val - current_best[0][0] < current_val or k > len(current_best)):
+                        self.stack.append(call + 1)
+                        self.access(WRITE, QUERY, call + 1, 0)
+                        self.backtrack()
+                        self.knn_rec(query, current_best, k, tree.left, level + 1)
+        else:
+            self.backtrack()
+        self.access(READ, QUERY, self.stack.pop(), 32)
+        
+    def exhaustive_search(self, query, current_best, bucket, k):
+        for p in bucket:
+            self.access(READ, POINT, self.point_indices[p], 0)
+            self.computation(18)
+            distance = -p.distance(query)
+            self.computation(4)
+            if len(current_best) == k:
+                if current_best[0][0] < distance:
+                    heapq.heapreplace(current_best, p)
+            else:
+                heapq.heappush(current_best, p)
+            
+    #Computes address for given address based on data type, data index, and offset, and writes it to the trace in a tuple
+    def access(self, access_type, data_type, index, offset):
+        address = self.memory.address(data_type, index, offset)
+        if not self.split:
+            address += self.memory_ptrs[data_type]
+        self.query_trace.add(("R", data_type, address))
+
+    #Adds desired number of non access instructions to trace
+    def computation(self, num):
+        for _ in range(num):
+            self.query_trace.add("C")
+    def backtrack(self):
+        self.query_trace.add("BT")
+    
+    def load(self, subtree):
+        self.query_trace.add(("L", subtree))
 
 
 #Internal node class
@@ -443,8 +597,6 @@ class Node:
             self.p = p
 
 
-#tree = KD_Tree("../kdTree_Inputs/test")
-#tree.query_trace = Query()
-#p = Point([24, 0, 1])
-#print(tree.knn(p, 2))
-
+tree = Bucket_KD_Tree("../kdTree_Inputs/test", 3)
+tree.memory = Memory()
+tree.knn(Point([0,0,0]), 2)
